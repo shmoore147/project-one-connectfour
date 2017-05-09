1b14340dac57c6065dc1ffb5dbc6b4f56938fd53
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-09 17:12:33 -0400

    still not alternating colors

--- projects/project-one-connectfour/script.js
+++ projects/project-one-connectfour/script.js
@@ -1,18 +1,11 @@
   $(function() {
-// begins the game by telling the players what to do next
 var start = $(".startButton");
     $(start).click(function () {
         alert("Click on a 'drop' button at the top of a column to drop your first chip!");
     });
 
-// when you click on a cell, the pink circle shows up there
 
- // $(".board").on('click', function (event) {
- //  //  console.log("hi!");
- //   $(event.target).append('<div class="chipPink"><div>');
- // });
-
-$(".dropButtonOne").on('click', function () {
+$("#dropButtonOne").on('click', function () {
   var counter = 2;
   counter += 1;
 
@@ -26,49 +19,69 @@ $(".dropButtonOne").on('click', function () {
 
 
 
-$(".dropButtonTwo").on('click', function () {
-  // console.log("hi!");
-  $("#forty-four").append('<div class="chipPink"></div>')
-});
 
-$(".dropButtonThree").on('click', function () {
-  // console.log("hi!");
-  $("#forty-five").append('<div class="chipPink"></div>')
-});
 
-$(".dropButtonFour").on('click', function () {
-  // console.log("hi!");
-  $("#forty-six").append('<div class="chipPink"></div>')
-});
 
-$(".dropButtonFive").on('click', function () {
-  // console.log("hi!");
-  $("#forty-seven").append('<div class="chipPink"></div>')
-});
-
-$(".dropButtonSix").on('click', function () {
-  // console.log("hi!");
-  $("#forty-eight").append('<div class="chipPink"></div>')
-});
-
-$(".dropButtonSeven").on('click', function () {
-  // console.log("hi!");
-  $("#forty-nine").append('<div class="chipPink"></div>')
-});
+// $("#dropButtonTwo").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
 //
-// $(".dropButtons").css("background-color", "yellow");
-//
-//
-//
-// $('#green').on('click', function () {
-//   $('#black').css('background-color', 'green');
+//       if (counter % 2 == 0) {
+//   $("#forty-four").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-four").append('<div class="chipPink"></div>');
+// }
 // });
-
-
-// $(“button”).click(function(){
-//     $(“p”).css(“color”, “red”);
+//
+// $("#dropButtonThree").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
+//
+//       if (counter % 2 == 0) {
+//   $("#forty-five").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-five").append('<div class="chipPink"></div>');
+// }
 // });
-
-
+// $("#dropButtonFour").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
+//
+//       if (counter % 2 == 0) {
+//   $("#forty-six").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-six").append('<div class="chipPink"></div>');
+// }
+// });
+// $("#dropButtonFive").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
+//
+//       if (counter % 2 == 0) {
+//   $("#forty-seven").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-seven").append('<div class="chipPink"></div>');
+// }
+// });
+// $("#dropButtonSix").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
+//
+//       if (counter % 2 == 0) {
+//   $("#forty-eight").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-eight").append('<div class="chipPink"></div>');
+// }
+// });
+// $("#dropButtonSeven").on('click', function () {
+//   var counter = 2;
+//   counter += 1;
+//
+//       if (counter % 2 == 0) {
+//   $("#forty-nine").append('<div class="chipYellow"></div>');
+// }  else{
+//         $("#forty-nine").append('<div class="chipPink"></div>');
+// }
+// });
 
   });
