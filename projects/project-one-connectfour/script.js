bf5c5bde9f7e2ea1bd4c1227f950b2e675c4d2f5
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-09 15:48:23 -0400

    added buttons that drop to bottom row

--- projects/project-one-connectfour/script.js
+++ projects/project-one-connectfour/script.js
@@ -2,40 +2,48 @@
 // begins the game by telling the players what to do next
 var start = $(".startButton");
     $(start).click(function () {
-        alert("Click on any box to drop your first chip!");
+        alert("Click on a 'drop' button at the top of a column to drop your first chip!");
     });
 
 // when you click on a cell, the pink circle shows up there
 
- $(".board").on('click', function (event) {
-  //  console.log("hi!");
-   $(event.target).append('<div class="chipPink"><div>');
- });
+ // $(".board").on('click', function (event) {
+ //  //  console.log("hi!");
+ //   $(event.target).append('<div class="chipPink"><div>');
+ // });
 
+$(".dropButtonOne").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
+$(".dropButtonTwo").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
+$(".dropButtonThree").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
+$(".dropButtonFour").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
+$(".dropButtonFive").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
+$(".dropButtonSix").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
 
-
-
- //Start game function. Puts 'pink' or 'yellow' in box. Switches from 'pink' to 'yellow' on each turn, plus incrementing the turn counter.
-     function startGame() {
-         $($gameCells).one('click', function() {
-             $(this).html(turn);
-             moves[this.id] = turn;
-             count++;
-             if (count % 2 === 0) {
-                 turn = 'pink'; //pink turn
-                 checkWinner('pink');
-             } else {
-                 turn = 'yellow'; //yellow turn
-                 checkWinner('yellow');
-             }
-         });
-         console.log(moves, turn, count);
-     }
-
-
+$(".dropButtonSeven").on('click', function () {
+  // console.log("hi!");
+  $("#forty-three").append('<div class="chipPink"></div>')
+});
   });
