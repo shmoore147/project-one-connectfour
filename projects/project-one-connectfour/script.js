e33baaf37e79822d765c35520d728798ceea20b1
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-09 14:55:56 -0400

    working on alternating players colors

--- projects/project-one-connectfour/script.js
+++ projects/project-one-connectfour/script.js
@@ -1,12 +1,41 @@
   $(function() {
-
+// begins the game by telling the players what to do next
 var start = $(".startButton");
     $(start).click(function () {
-        alert("The game is starting!");
+        alert("Click on any box to drop your first chip!");
     });
 
+// when you click on a cell, the pink circle shows up there
+
+ $(".board").on('click', function (event) {
+  //  console.log("hi!");
+   $(event.target).append('<div class="chipPink"><div>');
+ });
 
 
+
+
+
+
+
+
+
+ //Start game function. Puts 'pink' or 'yellow' in box. Switches from 'pink' to 'yellow' on each turn, plus incrementing the turn counter.
+     function startGame() {
+         $($gameCells).one('click', function() {
+             $(this).html(turn);
+             moves[this.id] = turn;
+             count++;
+             if (count % 2 === 0) {
+                 turn = 'pink'; //pink turn
+                 checkWinner('pink');
+             } else {
+                 turn = 'yellow'; //yellow turn
+                 checkWinner('yellow');
+             }
+         });
+         console.log(moves, turn, count);
+     }
 
 
   });
