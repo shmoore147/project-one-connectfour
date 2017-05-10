73427c5d5c618aa35a17c956d329842826341146
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-10 11:31:40 -0400

    colors are alternating

--- projects/project-one-connectfour/script.js
+++ projects/project-one-connectfour/script.js
@@ -1,87 +1,24 @@
-  $(function() {
-var start = $(".startButton");
+$(function() {
+    var start = $(".startButton");
     $(start).click(function () {
         alert("Click on a 'drop' button at the top of a column to drop your first chip!");
     });
 
-
-$("#dropButtonOne").on('click', function () {
-  var counter = 2;
-  counter += 1;
-
-      if (counter % 2 == 0) {
-  $("#forty-three").append('<div class="chipYellow"></div>');
-}  else{
-        $("#forty-three").append('<div class="chipPink"></div>');
-}
-});
+    var playerTurn = 1
 
 
+    $("#dropButtonOne").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-three').append('<div class="chipPink"></div>')
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-three').append('<div class="chipYellow"></div>')
+        }
+    });
 
-
-
-
-
-// $("#dropButtonTwo").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-four").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-four").append('<div class="chipPink"></div>');
-// }
-// });
-//
-// $("#dropButtonThree").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-five").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-five").append('<div class="chipPink"></div>');
-// }
-// });
-// $("#dropButtonFour").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-six").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-six").append('<div class="chipPink"></div>');
-// }
-// });
-// $("#dropButtonFive").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-seven").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-seven").append('<div class="chipPink"></div>');
-// }
-// });
-// $("#dropButtonSix").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-eight").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-eight").append('<div class="chipPink"></div>');
-// }
-// });
-// $("#dropButtonSeven").on('click', function () {
-//   var counter = 2;
-//   counter += 1;
-//
-//       if (counter % 2 == 0) {
-//   $("#forty-nine").append('<div class="chipYellow"></div>');
-// }  else{
-//         $("#forty-nine").append('<div class="chipPink"></div>');
-// }
-// });
-
-  });
+})
