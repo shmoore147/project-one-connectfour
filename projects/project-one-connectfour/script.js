d96f9122f55325e89550157dc319a3cbc8514901
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-12 13:31:29 -0400

    final commit with readme

--- projects/project-one-connectfour/script.js
+++ projects/project-one-connectfour/script.js
@@ -1,10 +1,24 @@
 $(function() {
     var start = $(".startButton");
     $(start).click(function () {
-        alert("Hi there! Player one: Click on a 'drop' button at the top of a column to drop your first chip!");
+        alert("Flip your laptop upside down to play! Player one: Click on a 'drop' button at the top of a column to drop your first chip. The first player to get four of the same color chips in a row is the winner!");
     });
 
     var playerTurn = 1
+
+
+
+
+    $('.board').on('click', function(event){
+        $(event.target).addClass(`player${currentPlayer}` );
+        if(currentPlayer === 1){
+            currentPlayer = 2
+        } else if (currentPlayer === 2){
+            currentPlayer = 1
+        }
+
+    });
+
 
 
     $("#dropButtonOne").on('click', function () {
@@ -13,6 +27,7 @@ $(function() {
           chipColor = "pink";
           playerTurn = 2
           $('#forty-three').append('<div class="chipPink"></div>')
+
         }
         else if (playerTurn === 2) {
           chipColor = "yellow";
@@ -20,5 +35,98 @@ $(function() {
           $('#forty-three').append('<div class="chipYellow"></div>')
         }
     });
+
+
+    $("#dropButtonTwo").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-four').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-four').append('<div class="chipYellow"></div>')
+        }
+    });
+
+    $("#dropButtonThree").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-five').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-five').append('<div class="chipYellow"></div>')
+        }
+    });
+    $("#dropButtonFour").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-six').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-six').append('<div class="chipYellow"></div>')
+        }
+    });
+    $("#dropButtonFive").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-seven').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-seven').append('<div class="chipYellow"></div>')
+        }
+    });
+
+
+    $("#dropButtonSix").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-eight').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-eight').append('<div class="chipYellow"></div>')
+        }
+    });
+
+
+    $("#dropButtonSeven").on('click', function () {
+        var chipColor = null
+        if (playerTurn === 1) {
+          chipColor = "pink";
+          playerTurn = 2
+          $('#forty-nine').append('<div class="chipPink"></div>')
+
+        }
+        else if (playerTurn === 2) {
+          chipColor = "yellow";
+          playerTurn = 1
+          $('#forty-nine').append('<div class="chipYellow"></div>')
+        }
+    });
+
+
 
 })
